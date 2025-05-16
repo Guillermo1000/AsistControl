e5c32343abe5ab09c63cc47e2bd8fd6da7372023
Guillermo1000 <guillermoajv20@gmail.com>
2025-05-16 18:35:00 -0400

    Se Diseño la pantalla principal

--- Presentacion/MenuPrincipal.cs
+++ Presentacion/MenuPrincipal.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace AsistControl
+namespace AsistControl.Presentacion
 {
-    public partial class Form1 : Form
+    public partial class MenuPrincipal : Form
     {
-        public Form1()
+        public MenuPrincipal()
         {
             InitializeComponent();
         }
